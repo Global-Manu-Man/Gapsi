efdf9c248a6160364dde5d7be01e3353482275cb
Global-Manu-Man <sandoval.morales.emmanuel@gmail.com>
2021-06-16 14:31:26 -0500

    Create store.java

---
+++ WalmartAPI/src/main/java/com/example/gradle/entitys/store.java
@@ -0,0 +1,61 @@
+package com.example.gradle.entitys;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "STORE")
+public class store {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "rate")
+    private int rate;
+    @Column(name = "model")
+    private String model;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+}
